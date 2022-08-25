c0f2bbad7af5846e2a344dddfea1de6c52142ae4
Asli2022 <ashleyaden4u@hotmail.com>
2022-08-25 19:42:33 +0100

    My First Javascript

---
+++ Syntax.js
@@ -0,0 +1,64 @@
+//This is a one line comment in javascript 
+
+/* this is used for multiple
+lines in javascript*/
+
+//Below is my first javascript code
+
+console.log ("Hello World!");
+
+console.log ("My name is Asli Aden");
+
+console.log ("I am learning JavaScript!");
+
+//print a number
+
+
+console.log (1);
+
+//print a string
+
+// A string is 
+
+console.log ("I enjoy learning new skills and applying them.");
+
+// let variable is 
+
+let age = 45;
+
+console.log (age);
+
+age =  55;
+
+console.log (age);
+
+//Make a constant variable
+
+const a = 3;
+
+/*const a = 5; This code with process an error as a was already set to 3 and this cannot be changed, 
+as const is fixed variable. */
+
+
+console.log (a);
+
+//print boolean (true) (false)
+
+let b = (1 > 3 )
+console.log (b);
+
+let c = (9 == 9)
+console.log (c);
+
+
+//create an undefined variable and print it off
+
+let first_name;
+console.log (first_name)
+
+
+
+//create a null variable and print it off
+
+let surname_name = null;
+console.log (surname_name)

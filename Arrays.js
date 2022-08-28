33250065b4684a89dd85c6bd833b0cd0617ad4a6
Asli2022 <ashleyaden4u@hotmail.com>
2022-08-28 10:36:25 +0100

    Arrays in JavaScript

---
+++ Arrays.js
@@ -0,0 +1,14 @@
+// Arrays are 
+
+// Arrays of string
+
+const AWS_Services  = [ "AWS", "VPC", "EC2", "ECS", "S3", "IAM", "CloudTrail", "Cloud9", "Codecommit", "EBS", "EFS", "ALB", "NLB", "NACLS" ]
+
+
+// Arrays of numbers
+
+const num = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15 ]
+
+// Arrays of mixed data types
+
+const datatypes = [ "Fred", 22, "Linda", 40, "Sarah", 38, "Abdi", 28, "Fatima", 25, "Sabrina", 25, "Farah", 21, "Dave" , 22 ]

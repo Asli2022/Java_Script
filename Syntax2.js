b2c749753dc66b5b7fddb0fc31ace93058d91609
Asli2022 <ashleyaden4u@hotmail.com>
2022-08-28 10:38:45 +0100

    Syntax Part 2

---
+++ Syntax2.js
@@ -0,0 +1,22 @@
+// Concatenating strings
+
+const msg = ("Hello") ;
+console.log (msg);
+
+const first_name = ("Asli");
+
+
+const last_name = ("Aden.");
+
+let country = ("Somalia.");
+
+let location = ("Birmingham.");
+
+console.log (("My name is ") + (first_name + " " + last_name)) ;
+
+
+
+console.log (("I was born in ") + " " + country )
+
+console.log (("I live in ") + " " + location);
+

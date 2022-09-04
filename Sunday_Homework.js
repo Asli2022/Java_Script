e8c7d2869b0764f59548720852db3bcbf78bfa87
Asli2022 <ashleyaden4u@hotmail.com>
2022-09-04 12:58:42 +0100

    .Sunday_Homework.

---
+++ Sunday_Homework.js
@@ -0,0 +1,71 @@
+//Home work due for Sunaday:
+
+//Part one
+
+//String concatenation 
+let message = ("My favourite food is");
+
+let food = ("rice and meat");
+
+console.log ((message + " " + food));
+
+//Part two
+
+//variable
+
+let num = (22);
+console.log (num);
+
+let greeting = ("Good evening everyone!!")
+console.log ("greeting");
+
+const cal = (10 == 10);
+console.log (cal);
+
+
+//Part 3
+
+// string interpolation 
+
+let university = ("Wolverhampton University");
+
+let degree = ("Bsc Nursing and Bsc Midwifery.");
+
+let completion = (2013);
+
+console.log (("I graduated from ") + (university ) + (" ") + ("in ")+ (completion) + (" ") + ("and" )+ (" ")+ ("studied ") + (" ") + (degree));
+
+//Part 4 
+
+//Conditional statement
+
+const day = ("Monday") ;
+
+const eat = ("rice and chicken.");
+
+if ( day == "Monday" ) {
+    
+    console.log (("Your lunch today is ") + (" ") + (eat));
+}
+else if ( day != "Monday") {
+    console.log ("Have something else for lunch. ");
+}
+
+
+
+//Part 5 
+
+//Arrays
+
+const shopping_list = ["shirts ", "trouser "];
+
+const result = shopping_list.toString().toLowerCase();
+
+console.log (shopping_list);
+
+shopping_list.push ("SOCKS");
+
+console.log(shopping_list.toString().toLowerCase()); ["socks"];
+
+
+
